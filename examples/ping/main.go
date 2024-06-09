0f838de6088e2a6bdf95b3a8ec3eadb754c2b67d
Floris Cornel <floris@cornel.email>
2024-06-09 11:42:03 +0100

    refactor examples/ping

--- examples/ping/main.go
+++ examples/ping/main.go
@@ -15,28 +15,21 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8001", nil))
 }
 
-// pingMessage is the message that we send to clients.
-type pingMessage struct {
-	Message string `json:"message"`
-	Counter int    `json:"counter"`
-}
-
 // handler is the HTTP handler that sends incremental updates to the client.
 // It sends a "ping" message every second.
 func handler(w http.ResponseWriter, r *http.Request) {
-	sw := sse.NewResponseWriter(w, sse.Options{})
-	fmt.Println("New client connected")
-
-	i := 0
+	opts := sse.Options{
+		ResponseStatus: http.StatusOK,
+		Encoding:       sse.EncodeNone,
+	}
+	sw := sse.NewResponseWriter(w, opts)
 
 	for {
-		i++
 		select {
 		case <-r.Context().Done():
-			fmt.Println("A client has disconnected.")
 			return
 		case <-time.After(1 * time.Second):
-			err := sw.Write("ping", pingMessage{"pong", i})
+			err := sw.Write("ping", "pong")
 			if err != nil {
 				fmt.Println("Error writing to client:", err)
 				return
